d42c982a65936ef5b7e2788876a34ad3d26b9f2c
prettyboy <francecar@me.com>
2025-02-18 18:35:04 +0100

    B: implemented left and right movement

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from constants import*
+from player import*
 pygame.init()
+
+clock = pygame.time.Clock()
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__== "__main__":
